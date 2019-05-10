a967cdc4cf0a523532029435aeb523f52346c232
Leonardo Reis <leo.reis@b2wdigital.com>
2019-05-09 21:06:42 -0300

    networking

---
+++ geniusTask/Networking.swift
@@ -0,0 +1,13 @@
+//
+//  Networking.swift
+//  geniusTask
+//
+//  Created by Leonardo Reis on 09/05/19.
+//  Copyright © 2019 Leonardo Reis. All rights reserved.
+//
+
+import UIKit
+
+class Networking: NSObject {
+
+}
